11315f41cc7130f83ad4d44188a5d2e238922a2d
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2022-03-04 17:07:42 +0100

    poke api react

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { findPokemonById } from "../services/pokemon.service";
+
 const PokemonDetails = () => {
+    const [pokemon, setPokemon] = useState({})
+    const {id} = useParams()
+
+    const fetchPokemon = async () => {
+        try {
+            const data = await findPokemonById(id)
+            setPokemon(data)
+        } catch (error) {
+            
+        }
+    }
 
     // useEffect
-    // create function into service findPokemonById
-    // service showPokemon
+    useEffect( () => {
+        fetchPokemon()
+    },[fetchPokemon])
+
+   
 
     return ( 
         <div>
             Pokemon details
+
+            <h1>{pokemon.name}</h1>
         </div>
      );
 }
--- src/services/pokemon.service.js
+++ src/services/pokemon.service.js
@@ -4,7 +4,12 @@ function findPokemonByName(pokemonName = "ditto"){
     return axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
       .then(res => res.data)
 }
+function findPokemonById(pokemonId){
+    return axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+      .then(res => res.data)
+}
 
 export {
-    findPokemonByName
+    findPokemonByName,
+    findPokemonById
 }
